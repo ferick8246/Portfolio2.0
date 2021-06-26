58eafabb930254517e5b872f69b17ac05816cd90
Erick Flore <ferick8246@gmail.com>
2021-06-26 13:54:53 -0500

    debugging

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,12 @@
 $(document).ready(() => {
-    AOS.init();
-    $(".body-transition").hide();
-  
-    let renderAnimation = () => {
-      intro = $("<h1>");
-      intro.addClass(
-        "space-mono accent-color px-18 skew rounded animated zoomIn slow intro-element"
-      );
-      intro.text("E");
-      let container = $("<div>");
-      container.addClass("lead-container");
-      let row = $("<div>");
-      row.addClass("row text-center d-flex justify-content-center");
-      let col = $("<div>");
-      col.addClass("col-md-12 d-flex justify-content-center");
-      col.append(intro);
-      row.append(col);
-      container.append(row);
-      $("body").append(container);
-    };
-    renderAnimation();
-  
-    let addZoomOut = () => {
-      intro.addClass("animated zoomOut blur");
-    };
+     AOS.init();
+    //  let addZoomOut = () => {
+    //   intro.addClass("animated zoomOut blur");
+    // };
   
     const renderAutoTyper = () => {
       const displayBio = $("#display-bio");
-      const words = "I build things for the web.".split("");
+      const words = "The web builder.".split("");
       let i = 0;
   
       let bioInterval = setInterval(() => {
@@ -35,7 +14,7 @@ $(document).ready(() => {
         if (!letters) {
           clearInterval(bioInterval);
         } else {
-          displayBio.append(letters);
+          displayBio.append(letters); 
           i++;
         }
       }, 100);
@@ -59,3 +38,25 @@ $(document).ready(() => {
         nav.toggleClass('scrolled', $(this).scrollTop() > nav.height() * 20);
       });
   });
+
+
+  // $(".body-transition").hide();
+  
+    // let renderAnimation = () => {
+    //   intro = $("<h1>");
+    //   intro.addClass(
+    //     "space-mono accent-color px-18 skew rounded animated zoomIn slow intro-element"
+    //   );
+    //   intro.text("E");
+    //   let container = $("<div>");
+    //   container.addClass("lead-container");
+    //   let row = $("<div>");
+    //   row.addClass("row text-center d-flex justify-content-center");
+    //   let col = $("<div>");
+    //   col.addClass("col-md-12 d-flex justify-content-center");
+    //   col.append(intro);
+    //   row.append(col);
+    //   container.append(row);
+    //   $("body").append(container);
+    // };
+    // renderAnimation();
